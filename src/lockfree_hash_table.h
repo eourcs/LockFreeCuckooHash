47bbc51ade0fe22074699a157fa9bcf8a1d34fdd
rcoker7 <rcoker37@gmail.com>
2017-04-14 15:30:09 -0400

    Added internal structure for lockfree_hash_table

--- src/lockfree_hash_table.h
+++ src/lockfree_hash_table.h
@@ -3,14 +3,28 @@
 
 #include "hash_table.h"
 
-struct Lockfree_hash_table : Hash_table {
+struct Hash_entry {
+  int key;
+  int val;
+};
 
+typedef std::pair<Hash_entry*, int> Count_ptr;
+
+struct Lockfree_hash_table : Hash_table {
   Lockfree_hash_table(int capacity);
   
   std::pair<int, bool> search(int key);
   void                 insert(int key, int val);
   void                 remove(int key);
+
+private:
+  Count_ptr *table[2];  
+
+  int hash1(int key);
+  int hash2(int key);
+  int find(int key, Count_ptr &ptr1, Count_ptr &ptr2);
+  void relocate(int which, int index);
+  void help_relocate(int which, int index, bool initiator);
+  void del_dup(int idx1, Count_ptr ptr1, int idx2, Count_ptr ptr2);
 };
-
-
 #endif
