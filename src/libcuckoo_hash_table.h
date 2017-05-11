7fa9719389b1ec7280ebdc88ea35de902c570de3
rcoker7 <rcoker37@gmail.com>
2017-05-11 12:30:00 -0400

    Disabled libcuckoo, Intel TBB incoming

--- src/libcuckoo_hash_table.h
+++ src/libcuckoo_hash_table.h
@@ -1,31 +1,22 @@
 #ifndef LIBCUCKOO_HASH_TABLE
 #define LIBCUCKOO_HASH_TABLE
 
-#include <libcuckoo/cuckoohash_map.hh>
 #include "hash_table.h"
 
 struct Libcuckoo_hash_table {
-  Libcuckoo_hash_table(int capacity, int thread_count) : map(capacity) { }
+  Libcuckoo_hash_table(int capacity, int thread_count) { }
 
   std::pair<int, bool> search(int key, int tid) {
-    try {
-      int val = map.find(key);
-      return std::make_pair(val, true);
-    } catch (std::exception e) {
-      return std::make_pair(0, false);  
-    }
+    return std::make_pair(0, false);
   }
 
   void                 insert(int key, int val, int tid) {
-    map.insert_or_assign(key, val);
   }
 
   void                 remove(int key, int tid) {
-    map.erase(key);
   }
 
 private:
-  cuckoohash_map<int, int> map;
   
 };
 
